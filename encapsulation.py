03fa244e460c80da8f1023448f5f88596ef7b044
rishi patel <rishi.patel9080@gmail.com>
2026-09-20 18:33:13 +0530

    Change balance attribute to private in Account class

--- encapsulation.py
+++ encapsulation.py
@@ -1,15 +1,15 @@
 class Account:
     def __init__(self,balance,account_no):
-        self.balance=balance
+        self.__balance=balance
         self.__account_no=account_no
     def debit(self,amount):
-        self.balance-=amount
+        self.__balance-=amount
         print(f"rs{amount} was debited")
     def credit(self,amount):
-        self.balance+=amount
+        self.__balance+=amount
         print(f"rs{amount} was credited")
     def get_balance(self):
-        print(f"balance:{self.balance}")
+        print(f"balance:{self.__balance}")
 acc1=Account(10000,1234567)
 acc1.debit(5000)
 acc1.credit(10000)
